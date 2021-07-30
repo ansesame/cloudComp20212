af76a1a316187ea170dfee0936811ede830f66ea
ansesame <33558005+ansesame@users.noreply.github.com>
2021-07-30 11:01:26 -0500

    Update TCPservidor.py

--- TCPservidor.py
+++ TCPservidor.py
@@ -6,11 +6,20 @@ servidorSocket.bind(('',servidorPuerto))
 servidorSocket.listen(1)
 print("El servidor está listo para recibir mensajes")
 while 1:
+    #Confirmación conexión
     conexionSocket, clienteDireccion = servidorSocket.accept()
     print("Conexión establecida con ", clienteDireccion)
+    mensajeBienvenida = """Bienvenido al minibanquito
+    Aquí puede:
+    Consultar su saldo [1]
+    Debitar de su cuenta [2]
+    Acreditar a la cuenta [3] """
+    conexionSocket.send(bytes(mensajeBienvenida, "utf-8"))
+
     mensaje = str( conexionSocket.recv(1024), "utf-8" )
     print("Mensaje recibido de ", clienteDireccion)
     print(mensaje)
+
     mensajeRespuesta = mensaje.upper()
     print(mensajeRespuesta)
     conexionSocket.send(bytes(mensajeRespuesta, "utf-8"))
